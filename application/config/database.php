a140e71df617afa6c42bd4a9b7c2ae82f813f4f5
hao <38733670@qq.com>
2018-08-27 14:19:34 +0800

    db

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'mysql';
-$db['default']['password'] = 'mysql';
-$db['default']['database'] = 'igfit_hk';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'new_';
 $db['default']['pconnect'] = TRUE;
